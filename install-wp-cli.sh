13fb171a2e1b31a679452c8087a4bb2fe1bde4f7
Jon goldberg <jon@palantetech.coop>
2015-01-08 11:36:50 -0500

    Add support for installing to zsh

--- install-wp-cli.sh
+++ install-wp-cli.sh
@@ -3,9 +3,16 @@
 #fix the permissions
 chmod +x wp-cli
 
-#set up the bash profile
-echo PATH=`pwd`:'$PATH' >> $HOME/.bash_profile
-echo source `pwd`/wp-completion.bash >> $HOME/.bash_profile
+#set up the bash/zsh profile
+if [[ -f $HOME/.bash_profile ]]; then
+  echo PATH=`pwd`:'$PATH' >> $HOME/.bash_profile
+  echo source `pwd`/wp-completion.bash >> $HOME/.bash_profile
+fi
+
+if [[ -f $HOME/.zshrc ]]; then
+  echo PATH=`pwd`:'$PATH' >> $HOME/.zshrc
+fi
+
 
 #create a wrapper to grab the CiviCRM class
 echo '#!/bin/sh' > wp
