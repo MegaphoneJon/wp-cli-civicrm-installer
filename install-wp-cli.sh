9bce07aed46a83d0929d7ccffc02fe5802130a5e
Jon goldberg <jon@palantetech.coop>
2016-05-04 21:51:38 -0400

    Update install-wp-cli to handle Ubuntu properly - see #26566

--- install-wp-cli.sh
+++ install-wp-cli.sh
@@ -3,10 +3,22 @@
 #fix the permissions
 chmod +x wp-cli
 
-#set up the bash/zsh profile
-echo PATH=`pwd`:'$PATH' >> $HOME/.bash_profile
-echo source `pwd`/wp-completion.bash >> $HOME/.bash_profile
+#set up the bash profile
+#Palante #26566 - handle Ubuntu differently than Debian
+UBUNTU=`lsb_release -i | grep -c Ubuntu`
+if [ "$UBUNTU" -gt 0 ]; then
+  BASHPROFILE="$HOME/.profile"
+else
+  BASHPROFILE="$HOME/.bash_profile"
+fi
+echo $BASHPROFILE
+echo done
+exit
 
+echo PATH=`pwd`:'$PATH' >> $BASHPROFILE
+echo source `pwd`/wp-completion.bash >> $BASHPROFILE
+
+#set up the zsh profile
 if [ -f $HOME/.zshrc ]; then
   echo PATH=`pwd`:'$PATH' >> $HOME/.zshrc
 fi
